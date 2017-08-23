7c3f239296d912a098d2d1501eeedc99cdce911f
joshiakshay91 <ajoshi6@mail.csuchico.edu>
2017-08-23 00:19:00 -0700

    updateing files

---
+++ website/index.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>My Shop</title>
+    </head>
+
+    <body>
+        <h1>Welcome to test shoping list</h1>
+        <h1>This list is called from a python_api on different docker host</h1>
+        <h4>
+        <ul>
+            <?php
+            $json = file_get_contents('http://py-service/');
+            $obj = json_decode($json);
+            $products = $obj->products;
+            foreach ($products as $product) {
+                echo "<li>$product</li>";
+            }
+            ?>
+        </ul>
+      </h4>
+    </body>
+</html>
